fac37c5f2fa2c600e7dd2bfde18854252b9a283a
night <github@durdom.com>
2026-02-15 20:24:22 -0800

    Add docker-build.sh to build axe-os dist files

--- main/http_server/axe-os/docker-build.sh
+++ main/http_server/axe-os/docker-build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker run --rm -u $(id -u):$(id -g) -v "$(pwd)":/app -w /app node:24-slim sh -c "npm i && npm run build"
+docker run --rm -u $(id -u):$(id -g) -v "$(pwd)":/app -w /app node:24-slim sh -c "npm i && npm run build-only"
 
 
