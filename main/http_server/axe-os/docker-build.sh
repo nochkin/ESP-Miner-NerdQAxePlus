12ffe2e02517aee81da2f2c5332886e397659657
nochkin <github@durdom.com>
2026-02-22 23:05:24 -0500

    axe-os: fix building on certain systems

--- main/http_server/axe-os/docker-build.sh
+++ main/http_server/axe-os/docker-build.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-docker run --rm -u $(id -u):$(id -g) -v "$(pwd)":/app -w /app node:24-slim sh -c "npm i && npm run build-only"
-
+docker run --rm -u $(id -u):$(id -g) -v "$(pwd)":/app -w /app node:24-slim sh -c "export HOME=/app && npm i && npm run build-only"
 
